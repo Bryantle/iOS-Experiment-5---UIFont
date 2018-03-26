a1b9ed3d0fae7613e062e19b498c463d9edfe9b1
Bryantle <bryantl@berkeley.edu>
2018-03-26 13:57:15 -0700

    Tried 6 different fonts using UIFont.

--- iOS Experiment 5 - UIFont/ViewController.swift
+++ iOS Experiment 5 - UIFont/ViewController.swift
@@ -9,9 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var labels: [UILabel]!
+    func updateFonts(){
+        labels[0].font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.headline)
+        labels[1].font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.subheadline)
+        labels[2].font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
+        labels[3].font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.footnote)
+        labels[4].font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.caption1)
+        labels[5].font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.caption2)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateFonts()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
